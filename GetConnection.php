30a83334db921a637b958a4963ae48f29d8ee849
Vicky AS <vickyhuang808@gmail.com>
2023-09-11 21:23:00 +0700

    ganti nama database

--- GetConnection.php
+++ GetConnection.php
@@ -4,7 +4,7 @@ function getConnection(): PDO
 {
     $host = "localhost";
     $port = 3306;
-    $database = "belajar_php_database";
+    $database = "belajar_php_db";
     $username = "root";
     $password = "";
 
--- TestConnection.php
+++ TestConnection.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $port = 3306;
-$database = "belajar_php_database";
+$database = "belajar_php_db";
 $username = "root";
 $password = "";
 
